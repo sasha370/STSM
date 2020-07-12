69d362c8ee183084e298730ed6d44b1ed7993978
sasha370 <kng.sasha@mail.ru>
2020-07-12 19:51:31 +0300

    add Alert for Seach

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,24 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
-  end
 
+    # Первая проверка: передал ли ПОИСК в params хоть чтото?
+    if params[:stock].present?
+      # Если есть такой атрибут, то создаем с помощью модели Переменную или получаем NIL, если с сайта пришла ошибка
+      @stock = Stock.new_lookup(params[:stock])
+      # Проверяем создалась ли переменная, если Даб то отрисовываем результат
+      if @stock
+        render 'users/my_portfolio'
+      else
+        # если нет, то перенаправляем на новый ввод и сообщаем о ошибке
+        flash[:alert] = "Пожалуйста введите ПРАВИЛЬНОЕ название Акции"
+        redirect_to my_portfolio_path
+      end
+      # Если в params отсутвует поле stock, то значит был поиск по пустой строке
+    else
+      flash[:alert] = "Пожалуйста введите название Акции"
+      redirect_to my_portfolio_path
+    end
+  end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,23 @@
 class Stock < ApplicationRecord
 
+  # Данная модель создает для себя переменную, делая запрос на сайт.
+      # и методом new создается новый экземпляр класса, в который записываются поля в соответсвии с Переменной Client
+
+
   def self.new_lookup(ticker_symbol)
 
     client = IEX::Api::Client.new(
         publishable_token: 'Tpk_ee1d714f88de4145a248e4dc29150ed9',
         secret_token: 'Tsk_36dc4c7f7e59476b8bc89e8b8787dc46',
         endpoint: 'https://sandbox.iexapis.com/v1')
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
 
+    # Метод для выхода, если введены некоректные данные
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      # если пришла ошибка в данных, то возвращаем nil, который затем анализируем в контроллере
+      return nil
+    end
   end
 
 
