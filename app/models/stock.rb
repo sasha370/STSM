f4c122b2404ea07946ca03f639e0b54a66f22a5f
sasha370 <kng.sasha@mail.ru>
2020-07-12 19:26:07 +0300

    add display Search result

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,9 +3,11 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
 
     client = IEX::Api::Client.new(
-        publishable_token: 'pk_452b528c3d524ff19a7631634ec4f5f8',
-        endpoint: 'https://cloud.iexapis.com/v1')
-    client.price(ticker_symbol)
+        publishable_token: 'Tpk_ee1d714f88de4145a248e4dc29150ed9',
+        secret_token: 'Tsk_36dc4c7f7e59476b8bc89e8b8787dc46',
+        endpoint: 'https://sandbox.iexapis.com/v1')
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+
   end
 
 
