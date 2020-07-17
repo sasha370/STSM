015fee76116943349ab442a744959fe080456ca1
sasha370 <kng.sasha@mail.ru>
2020-07-17 14:49:39 +0300

    add Delete and Add Friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,16 @@
 class FriendshipsController < ApplicationController
   def create
+    # парсим переменную из переданных данных
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Вы успешно подписались на #{friend.first_name}"
+    else
+      flash[:alert] = "Что-то пошло не так"
+    end
+    redirect_to my_friends_path
+
+
   end
 
   def destroy
