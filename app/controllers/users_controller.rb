8b66a664b68546353f367e4046c70b45fb5ce87b
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:39:54 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,12 @@
 class UsersController < ApplicationController
 
-
   def show
     @user = User.find(params[:id])
     @tracked_stocks = @user.stocks
   end
 
-
-
   def my_portfolio
-    # Выбирем все акции, которые привызанны к текущему пользователю. Current_user обеспечен Devise
     @tracked_stocks = current_user.stocks
-    # Данная переменная нужна, чтобы понимать, что @user - это текущий пользователь и ему можно показывать некоторые кнопки
     @user = current_user
   end
 
@@ -20,25 +15,19 @@ class UsersController < ApplicationController
   end
 
   def search
-    # Первая проверка: передал ли ПОИСК в params хоть чтото?
     if params[:friend].present?
-      # Если есть такой атрибут, то создаем с помощью модели Переменную или получаем NIL, если с сайта пришла ошибка
-      # Используя метод Search из User.rb создаем список найденых друзей
       @friends = User.search(params[:friend])
-      # Данный метод удаляет текущего пользователя из результатов поиска
       @friends = current_user.except_current_user(@friends)
-      # Проверяем создалась ли переменная, если Даб то отрисовываем результат
       if @friends.empty?
         respond_to do |format|
           flash.now[:alert] = "Никого не найдено по этим данным"
           format.js { render partial: 'friends/result' }
         end
-      else # Если переменная создалась, то отправляем данные в форму для JS
+      else
         respond_to do |format|
           format.js { render partial: 'friends/result' }
         end
       end
-      # Если в params отсутвует поле friend, то значит был поиск по пустой строке
     else
       respond_to do |format|
         flash.now[:alert] = "Пожалуйста введите Имя или Email друга"
@@ -46,6 +35,4 @@ class UsersController < ApplicationController
       end
     end
   end
-
-
 end
