55e8d5df0792715eb553103810365c72329a8fa9
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:46:43 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  # из модели Юзер-Акции нам нужно тольеко метод Create и дестрой остальные отключаем
   resources :user_stocks, only: [:create, :destroy]
-
-  # Добавить в друзья и удалить реализовано через Friendship
-  # После генерации удалил Вьюхи и GETs которые прописались автоматом
   resources :friendships, only: [:create, :destroy]
   resources :users, only: [:show], :constraints => { :id => /[0-9|]+/ }
 
@@ -13,9 +9,6 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
 
-  # Поиск реализован через контроллер USER
   get 'search_friend', to: 'users#search'
-
-
   devise_for :users
 end
