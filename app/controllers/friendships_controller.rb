35374ffb3c0431d9d808c2ce6fb32e787db525a6
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:38:19 +0300

    Update friendships_controller.rb

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,5 @@
 class FriendshipsController < ApplicationController
   def create
-    # парсим переменную из переданных данных
     friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: friend.id)
     if current_user.save
@@ -9,16 +8,12 @@ class FriendshipsController < ApplicationController
       flash[:alert] = "Что-то пошло не так"
     end
     redirect_to my_friends_path
-
-
   end
 
   def destroy
-    # отыскиваем нашу Акцию в БД по переданному ID в url
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.delete
     flash[:notice]= "Вы отписались"
     redirect_to my_friends_path
   end
-
 end
